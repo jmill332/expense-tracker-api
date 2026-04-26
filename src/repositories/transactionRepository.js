50b13a39e284f77d09fe23c42635fc9d41452d53
JMill332 <josh4chicagobears@gmail.com>
2026-04-26 16:05:10 -0400

    prisma changes

--- src/repositories/transactionRepository.js
+++ src/repositories/transactionRepository.js
@@ -1,10 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 const createTransaction = async (amount, description, categoryId, userId, type) => {
   return await prisma.transaction.create({
